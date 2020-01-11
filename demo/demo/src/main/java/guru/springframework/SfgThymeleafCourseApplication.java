fb636654d1c2574a73af42a7e0a7af92e09aaf23
Anna Salcedo <salcedoanna8@gmail.com>
2020-01-11 17:36:48 -0500

    First Commit

--- demo/demo/src/main/java/guru/springframework/SfgThymeleafCourseApplication.java
+++ demo/demo/src/main/java/guru/springframework/SfgThymeleafCourseApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SfgThymeleafCourseApplication {
-
+kot
 	public static void main(String[] args) {
 		SpringApplication.run(SfgThymeleafCourseApplication.class, args);
 	}
